8e733f8237ba07800a8e982d7303b369a02ae8ea
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2022-08-02 17:05:52 -0300

    Added calculations

---
+++ src/controllers/calculator.ts
@@ -0,0 +1,44 @@
+import { Inject } from "@decorators/di";
+import { Controller, Post, Request, Response } from "@decorators/express";
+import { Request as ExpressRequest, Response as ExpressResponse } from "express";
+import { CarbonData } from "../models";
+import { TimeConverterService } from "../services/timeConverter";
+import { UnitConverterService } from "../services/unitConverter";
+
+@Controller('/calculate')
+export class CalculatorController {
+    constructor(
+        @Inject(TimeConverterService) private timeConverterService : TimeConverterService,
+        @Inject(UnitConverterService) private unitConverterService : UnitConverterService
+    ) {}
+
+    @Post('')
+    calculate(@Request() req: ExpressRequest, @Response() res: ExpressResponse): void {
+        let carbonData = req.body as CarbonData[];
+
+        // Convert unit
+        carbonData = carbonData.map((data) => ({
+            ...data,
+            value: this.unitConverterService.convert(data.value, data.unit_type, data.usage.unit)
+        }));
+
+        // Convert time
+        carbonData = carbonData.map((data) => ({
+            ...data,
+            value: this.timeConverterService.convert(data.value, data.time_type)
+        }));
+
+        // Calculate by the Factor
+        const output = carbonData.map((data) => ({
+            name: data.usage.name,
+            value: data.value * data.usage.emission_factor
+        }))
+
+        const sum = output.reduce((sum, data) => sum + data.value, 0);
+
+        res.send({
+            sum,
+            data: output
+        })
+    }
+}
